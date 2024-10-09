60b2fce3ece17aa8d773098c10b1161fbf1d3c5c
j-about <142051449+j-about@users.noreply.github.com>
2024-10-09 13:04:30 +0000

    Score & Session

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,20 @@
 # pylint: disable=missing-docstring
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
 from longest_word.game import Game
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route('/')
 def home():
+    if not session.get("score"):
+        session["score"] = 0
     game = Game()
-    return render_template('home.html', grid=game.grid)
+    return render_template('home.html', grid=game.grid, score = session['score'])
 
 @app.route('/check', methods=["POST"])
 def check():
@@ -15,4 +22,13 @@ def check():
     game.grid = list(request.form['grid'])
     word = request.form['word']
     is_valid = game.is_valid(word)
-    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+    if not session.get("score"):
+        session["score"] = 0
+    if is_valid:
+        session["score"] += 1
+    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word, score = session['score'])
+
+@app.route('/reset')
+def reset():
+    session["score"] = 0
+    return redirect("/")
